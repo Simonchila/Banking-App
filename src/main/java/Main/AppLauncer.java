3cdd1c88917a9caef8e0539add285b670b30b6a7
Simonchila <simonchila47@gmail.com>
2025-04-06 11:32:16 +0200

    Finished step 1 simple change to project

--- src/main/java/Main/AppLauncer.java
+++ src/main/java/Main/AppLauncer.java
@@ -1,13 +1,8 @@
 package Main;
 
-
 import GUIs.LoginGui;
-
-
 import javax.swing.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class AppLauncer {
     public static void main(String[] args) {
         // use invokeLater to make updates to the GUI more thread-safe
